fbb9b4ce928df17f74c45a70f3a48d4e0fd9662c
Thomas hale <TH146688@davenportschools.org>
2019-09-30 09:32:25 -0500

    update change

--- src/Assignment/CondoSales.java
+++ src/Assignment/CondoSales.java
@@ -6,6 +6,7 @@ public class CondoSales {
 
     public static void main(String[] args) {
         int choice;
+        int houseValue = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Which condo so you prefer \n 1 - for Park view \n " +
                 "2 - for Golf course \n 3 - for lake view \nChoice >>>>>");
@@ -13,12 +14,47 @@ public class CondoSales {
 
         if (choice == 1) {
             System.out.println("A Park View condo is $150,000");
+            houseValue = 150000;
 
         }
-        if (choice == 1) {
+        else if (choice == 2) {
             System.out.println("A Golf course is $170,000");
+            houseValue = 170000;
 
         }
+        else if (choice == 3) {
+            System.out.println("A Lake view is $210,000");
+            houseValue = 210000;
+
+        } else if (choice > 3)
+            System.out.println("$0");
+        AddOnChoices(houseValue);
+
 
     }
+
+
+    public static void AddOnChoices(int houseValue) {
+
+        Scanner input = new Scanner(System.in);
+        int choice;
+        int total;
+
+
+        System.out.println("Which do you want to add on \n 1 - for Garage \n " +
+                "2 - for Parking space \nChoice >>>>>");
+        choice = input.nextInt();
+            total = houseValue + 5000;
+        if (choice == 1) {
+            System.out.println("A garage is an extra $5,000");
+            total = houseValue + 5000;
+        }
+       else if (choice == 2) {
+            System.out.println("A parking space is an extra $5,000");
+            total = houseValue + 5000;
+        }
+        System.out.println("The total is going to be " + total);
+    }
 }
+
+
